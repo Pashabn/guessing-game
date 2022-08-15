3ccbe7920e64bbc2c0f3ee0e9acd83e830a87fbd
github-username <72346436+Pashabn@users.noreply.github.com>
2022-08-15 23:26:08 +0300

    feat: added body of function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.left = min
+        this.right = max
     }
 
     guess() {
-
+        this.mid = Math.round((this.right - this.left) / 2) + this.left
+        return this.mid
     }
 
     lower() {
-
+        this.right = this.mid
     }
 
     greater() {
-
+        this.left = this.mid
     }
 }
 
